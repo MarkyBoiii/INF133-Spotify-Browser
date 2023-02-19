b150ecd6df2a2be8dd3a228d2bfb7c1ca35c1077
Marc Hagoriles <markyboyph@gmail.com>
2023-02-19 01:07:36 -0800

    Finished Part 1 and Loading Profile Info for Part 2

--- client/src/app/components/about/about.component.ts
+++ client/src/app/components/about/about.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { SpotifyService } from 'src/app/services/spotify.service';
 
 @Component({
   selector: 'app-about',
@@ -11,7 +12,9 @@ export class AboutComponent implements OnInit {
   profile_link:string = null;
 
   //TODO: inject the Spotify service
-  constructor() { }
+  constructor(private SpotifyService: SpotifyService) {
+    console.log(SpotifyService.aboutMe());
+   }
 
   ngOnInit() {
   }
@@ -19,4 +22,13 @@ export class AboutComponent implements OnInit {
   /*TODO: create a function which gets the "about me" information from Spotify when the button in the view is clicked.
   In that function, update the name, profile_pic, and profile_link fields */
 
+  loadProfileInfo() {
+    //Once SpotifyService gets a response from accessing info about aboutMe(), then replace name, profile_pic and profile_link with the response's
+    this.SpotifyService.aboutMe().then((response)=> {
+      //Because of the console.log on the constructor, we figured out the data that we get from the API request: name, imageURL, and spotifyProfile.
+      this.name = response.name;
+      this.profile_pic = response.imageURL;
+      this.profile_link = response.spotifyProfile;
+    })
+  }
 }
